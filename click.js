b38e9b3fe880e07d875fab0d81e40fc2555f3678
Dominic Tarr <dominic.tarr@gmail.com>
2016-03-06 17:11:59 +1300

    padding

--- click.js
+++ click.js
@@ -1,4 +1,4 @@
 module.exports = function click(name, action) {
-  return h('a', {href: '#', onclick: action}, name)
+  return h('a', {href: '#', onclick: action, style: { margin: '5px'}}, name)
 }
 
