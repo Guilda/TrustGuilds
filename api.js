71141ca6566c02b01adcea3cbeadcdcbaa122562
Dominic Tarr <dominic.tarr@gmail.com>
2016-04-14 16:08:34 +1200

    seprate non browser parts

--- api.js
+++ api.js
@@ -27,28 +27,6 @@ module.exports = function (sbot) {
   }
 }
 
-if(!module.parent) {
-  var opts = require('minimist')(process.argv.slice(3))
-  var cmd = process.argv[2]
-  delete opts._
-//  delete opts.$0
-  console.log(opts, Object.keys(opts).length)
-  if(Object.keys(opts).length == 0)
-    opts = process.argv[3]
-
-  require('ssb-client')(function (err, sbot) {
-    pull(
-      module.exports(sbot)[cmd](opts),
-      require('pull-stringify')('', '\n', '\n\n', 2),
-      pull.drain(process.stdout.write.bind(process.stdout), function (err) {
-        if(err && err !== true) throw err
-      })
-    )
-  })
-
-}
-
-
 
 
 
